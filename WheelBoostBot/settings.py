cb3645c93ef696d43221179fdbd39eedd2ebf724
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-22 16:26:23 +0300

    added ip

--- WheelBoostBot/settings.py
+++ WheelBoostBot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0yu@y6^@5)*6ucqu+qclu%)7o4=zm9cjb$9ga75=vn*($24na2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["134.122.106.85"]
+ALLOWED_HOSTS = ["178.128.155.88"]
 
 
 # Application definition
